952a9870a5c4d727b87daa9720e214549a0f6902
Cozma Rares <cozmarares11@gmail.com>
2025-05-11 13:45:13 +0300

    fix: return error if rd parser does not have grammar attributes

--- OxyMP/oxymp/src/data/rd_parser.rs
+++ OxyMP/oxymp/src/data/rd_parser.rs
@@ -10,19 +10,17 @@ const ATTRIBUTES: &[&str] = &["grammar"];
 pub(super) fn process_rd_parser(item: syn::Item) -> syn::Result<(syn::ItemMod, RDParserData)> {
     let (item, mut found_attrs) = process_module_helper(item, ATTRIBUTES)?;
 
-    let grammar_attrs = found_attrs
-        .remove("grammar")
-        .expect("grammar attr should be set");
+    let Some(grammar_attrs) = found_attrs.remove("grammar") else {
+        return Err(syn::Error::new(
+            item.ident.span(),
+            "Missing grammar attributes. A parser must have at least one grammar attribute.",
+        ));
+    };
 
     let grammar_rules = grammar_attrs
         .into_iter()
         .map(|(span, rule)| GrammarData { span, rule })
         .collect();
 
-    Ok((
-        item,
-        RDParserData {
-            grammar_rules,
-        },
-    ))
+    Ok((item, RDParserData { grammar_rules }))
 }
